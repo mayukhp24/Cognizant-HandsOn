8eefc4ce446ae721fa08fbf5a8d6d42c0e56b646
mayukhp24 <163581266+mayukhp24@users.noreply.github.com>
2026-06-25 00:53:50 +0530

    Implement Singleton pattern for Logger class

--- Week 1/Design Patterns/Singleton Pattern/singleton.java
+++ Week 1/Design Patterns/Singleton Pattern/singleton.java
@@ -1,1 +1,28 @@
+class Logger {
+    private static Logger instance;
+    private Logger() {
+    }
 
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
+
+public class SingletonPatternExample {
+    public static void main(String[] args) {
+        Logger log1 = Logger.getInstance();
+        Logger log2 = Logger.getInstance();
+
+        log1.log("Application started");
+        log2.log("Doing some work");
+
+        System.out.println("Same instance? " + (log1 == log2));
+    }
+}
