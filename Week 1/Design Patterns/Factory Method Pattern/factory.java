0b12991af3a9a29bf6ab1cb99b3617edca1f619b
mayukhp24 <163581266+mayukhp24@users.noreply.github.com>
2026-06-25 00:54:23 +0530

    Update factory.java

--- Week 1/Design Patterns/Factory Method Pattern/factory.java
+++ Week 1/Design Patterns/Factory Method Pattern/factory.java
@@ -1,1 +1,59 @@
+interface Document {
+    void open();
+}
 
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening a Word document");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening a PDF document");
+    }
+}
+
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening an Excel document");
+    }
+}
+
+abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+
+class WordFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
+
+public class FactoryMethodPatternExample {
+    public static void main(String[] args) {
+        DocumentFactory factory = new WordFactory();
+        Document doc = factory.createDocument();
+        doc.open();
+
+        factory = new PdfFactory();
+        doc = factory.createDocument();
+        doc.open();
+
+        factory = new ExcelFactory();
+        doc = factory.createDocument();
+        doc.open();
+    }
+}
