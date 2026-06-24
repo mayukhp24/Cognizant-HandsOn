3b94fd7a20105260e3c45e2929a52132f3a0e44a
mayukhp24 <163581266+mayukhp24@users.noreply.github.com>
2026-06-25 00:55:45 +0530

    Add Decorator Pattern implementation for notifications
    Implemented the Decorator Pattern with Notifier interface and decorators for SMS and Slack notifications.

---
+++ Week 1/Design Patterns/Decorator Pattern/decorator.java
@@ -0,0 +1,53 @@
+interface Notifier {
+    void send(String message);
+}
+
+class EmailNotifier implements Notifier {
+    public void send(String message) {
+        System.out.println("Email sent: " + message);
+    }
+}
+
+abstract class NotifierDecorator implements Notifier {
+    protected Notifier notifier;
+
+    public NotifierDecorator(Notifier notifier) {
+        this.notifier = notifier;
+    }
+
+    public void send(String message) {
+        notifier.send(message);
+    }
+}
+
+class SMSNotifierDecorator extends NotifierDecorator {
+    public SMSNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    public void send(String message) {
+        super.send(message);
+        System.out.println("SMS sent: " + message);
+    }
+}
+
+class SlackNotifierDecorator extends NotifierDecorator {
+    public SlackNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    public void send(String message) {
+        super.send(message);
+        System.out.println("Slack message sent: " + message);
+    }
+}
+
+public class DecoratorPatternExample {
+    public static void main(String[] args) {
+        Notifier notifier = new EmailNotifier();
+        notifier = new SMSNotifierDecorator(notifier);
+        notifier = new SlackNotifierDecorator(notifier);
+
+        notifier.send("Server is down!");
+    }
+}
